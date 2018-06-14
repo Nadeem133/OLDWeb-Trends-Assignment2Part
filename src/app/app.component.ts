9deb63c042b1b8c5ce821b2f2bcbf16fd6762ad2
Nadeem133 <32464612+Nadeem133@users.noreply.github.com>
2018-06-14 17:01:39 -0400

    Ver 1.1 - Few code changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 
+/*This displays my favourite heroes list. An array of four heroes has been created and 
+that is displayed. IN order to validate a stetemnt about the hero, a true false is setup
+which highlights the usage of boolean datatype. */
+
 @Component({
   selector: 'app-root',
   template: `
@@ -9,10 +13,8 @@ import { Hero } from './hero';
   <p>Heroes:</p>
   <ul>
     <li *ngFor="let hero of heroes">
-      {{ hero.title}} 
-      {{ hero.name}} 
-      aged {{ hero.age}}
-      was cricket champion is a {{hero.status}} statement.
+      {{ hero.title}} {{ hero.name}} aged {{ hero.age}}
+      was a CRICKET champion is a {{hero.status}} statement.
       </li>
   </ul>
   <p *ngIf="heroes.length > 3">There are many heroes in my list!</p>
@@ -22,10 +24,10 @@ import { Hero } from './hero';
 export class AppComponent {
   title = 'List of My Heroes';
   heroes = [
-    new Hero(1, 'Muhammad Ali', 'Mr.', false, 78),
-    new Hero(2, 'Ian Botham', 'Mr.', true, 62),
-    new Hero(3, 'Don Bradman', 'Mr.', true, 92),
-    new Hero(4, 'Angelina Julie', 'Ms.', false, 43)
+    new Hero(1, 'MUHAMMAD ALI', 'Mr.', false, 78),
+    new Hero(2, 'IAN BOTHAM', 'Mr.', true, 62),
+    new Hero(3, 'DON BRADMAN', 'Mr.', true, 92),
+    new Hero(4, 'ANGELINA JULIE', 'Ms.', false, 43)
   ];
 
   myHero = this.heroes[0];
